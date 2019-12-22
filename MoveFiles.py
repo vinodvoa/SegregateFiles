d7ea054a077fbd1f723726ebb7c295c20bf6c68d
vinodvoa <vinodvoa@yahoo.com.sg>
2019-12-22 17:51:55 +0800

    Added heic and xmp handling

--- MoveFiles.py
+++ MoveFiles.py
@@ -1,10 +1,11 @@
 ######################################################################
-# Function		:
+# Function		: Create dirs based on image file extensions and move
+#                 the corresponding files to those dirs
 # Date			: 5-Jun-17
 # Improvements	:
-# 					1. Function
-# 					2. Process CR2, DNG and move to RAW
-# 					3. Process TIFF and move to LR
+# 				1. Function
+# 				2. Process CR2, DNG and move to RAW dir
+# 				3. Process TIFF and move to TIFF dir
 ######################################################################
 
 import sys
@@ -14,8 +15,9 @@ import os.path
 import glob
 import shutil
 
-######################################################################
-
+jpglist = ['.jpg', '.JPG', '.jpeg', '.JPEG']
+rawlist = ['.NEF', '.CR2', '.dng', '.DNG','.heic','.HEIC']
+tiflist = ['.tif', '.TIF', '.tiff', '.TIFF']
 
 def createDir(cpath, ctype):
     destdir = os.path.join(cpath, ctype.upper())
@@ -29,15 +31,9 @@ def createDir(cpath, ctype):
 
     return (destdir)
 
-######################################################################
-
-
 def printMsg(fname, dirname):
     path, filename = os.path.split(fname)
-    print('%s file moved to %s' % (filename, dirname))
-
-######################################################################
-
+    print('%s file moved to %s' % (path + filename, dirname))
 
 def moveFiles(path, destdir):
     try:
@@ -57,102 +53,99 @@ def moveFiles(path, destdir):
         if (movecount > 0):
             print('%s file(s) moved' % movecount)
 
-######################################################################
+if __name__ == '__main__':
+    # platfrm = platform.system()
+    # print (platform.system())
 
-# platfrm = platform.system()
-# print (platform.system())
+    # set working dir
+    if (len(sys.argv) == 1):
+        # if (platfrm == "Windows")
+        # path = 'C:\\Users\\Vinod\\Dropbox\\Python\\Learning\\Completed\\MoveFiles\\'
+        path = '/Volumes/Seagate Working Disk/My Photos/Singapore/Gardens By The Bay 2017'
+        # else:
+        # path = '/Users/vinodverghese/Dropbox/'
+    elif (len(sys.argv) > 1):
+        path = str(sys.argv[1])
 
+        if not (os.path.exists(path)):
+            print('%s does not exist' % path)
+            sys.exit(99)
 
-# set working dir
-if (len(sys.argv) == 1):
-    # if (platfrm == "Windows")
-    # path = 'C:\\Users\\Vinod\\Dropbox\\Python\\Learning\\Completed\\MoveFiles\\'
-    path = '/Volumes/Seagate Working Disk/My Photos/Rahul NUS Graduation 2018/'
-    # else:
-    # path = '/Users/vinodverghese/Dropbox/'
-elif (len(sys.argv) > 1):
-    path = str(sys.argv[1])
+    print('Working directory : %s' % path)
+    print()
 
-    if not (os.path.exists(path)):
-        print('%s does not exist' % path)
-        sys.exit(99)
+    # get all files in path
+    allfiles = os.listdir(path)
 
-print('Working directory : %s' % path)
-print()
+    # check file extension and set flag
+    lrflag = False
+    jpgflag = False
+    rawflag = False
+    tifflag = False
 
-# check file extension and set flag
-LRflag = False
-jpgflag = False
-rawflag = False
-tifflag = False
+    # get file extensions and set flags
+    for file in allfiles:
+        if (file[:3] == 'LR-') and (not lrflag):
+            lrflag = True
 
-jpglist = ['.jpg', '.JPG', '.jpeg', '.JPEG']
-rawlist = ['.NEF', '.CR2', '.dng', '.DNG']
-tiflist = ['.tif', '.TIF', '.tiff', '.TIFF']
+        ext = os.path.splitext(file)[1]
 
-# get all files in path
-allfiles = os.listdir(path)
+        if (ext in jpglist) and (not jpgflag):
+            jpgflag = True
 
-# get file extensions and set flags
-for file in allfiles:
-    if (file[:3] == 'LR-') and (not LRflag):
-        LRflag = True
+        if (ext in rawlist) and (not rawflag):
+            rawflag = True
 
-    ext = os.path.splitext(file)[1]
+        if (ext in tiflist) and (not tifflag):
+            tifflag = True
 
-    if (ext in jpglist) and (not jpgflag):
-        jpgflag = True
+    # create and move files based on the flags
+    # print(lrflag)
+    # print(jpgflag)
+    # print(rawflag)
 
-    if (ext in rawlist) and (not rawflag):
-        rawflag = True
+    # create LR directory and move LR-* files
+    if (lrflag):
+        destdir = createDir(path, 'LR')
 
-    if (ext in tiflist) and (not tifflag):
-        tifflag = True
+        extn = 'LR-*'
 
-# print(LRflag)
-# print(jpgflag)
-# print(rawflag)
+        LRdir = os.path.join(path, extn)
+        moveFiles(LRdir, destdir)
 
-# create LR directory and move LR-* files
-if (LRflag):
-    destdir = createDir(path, 'LR')
+    # create JPG directory and move jpg files
+    if (jpgflag):
+        destdir = createDir(path, 'JPG')
 
-    extn = 'LR-*'
+        for ext in jpglist:
+            extn = '*' + ext
 
-    LRdir = os.path.join(path, extn)
-    moveFiles(LRdir, destdir)
+            jpgdir = os.path.join(path, extn)
+            # print(jpgdir)
+            moveFiles(jpgdir, destdir)
 
-# create JPG directory and move jpg files
-if (jpgflag):
-    destdir = createDir(path, 'JPG')
+    # create RAW directory and move raw files
+    if (rawflag):
+        destdir = createDir(path, 'RAW')
 
-    for ext in jpglist:
-        extn = '*' + ext
+        for ext in rawlist:
+            extn = '*' + ext
 
-        jpgdir = os.path.join(path, extn)
-        # print(jpgdir)
-        moveFiles(jpgdir, destdir)
+            rawdir = os.path.join(path, extn)
+            # print(rawdir)
+            moveFiles(rawdir, destdir)
 
-# create RAW directory and move raw files
-if (rawflag):
-    destdir = createDir(path, 'RAW')
+            rawdir = os.path.join(path, '*.xmp')
+            # print(rawdir)
+            moveFiles(rawdir, destdir)
 
-    for ext in rawlist:
-        extn = '*' + ext
+    # create TIFF directory and move tiff files
+    if (tifflag):
+        destdir = createDir(path, 'TIFF')
 
-        rawdir = os.path.join(path, extn)
-        # print(rawdir)
-        moveFiles(rawdir, destdir)
+        for ext in tiflist:
+            extn = '*' + ext
 
-# create TIFF directory and move tiff files
-if (tifflag):
-    destdir = createDir(path, 'TIFF')
-
-    for ext in tiflist:
-        extn = '*' + ext
-
-        rawdir = os.path.join(path, extn)
-        # print(rawdir)
-        moveFiles(rawdir, destdir)
-
-######################################################################
+            rawdir = os.path.join(path, extn)
+            # print(rawdir)
+            moveFiles(rawdir, destdir)
